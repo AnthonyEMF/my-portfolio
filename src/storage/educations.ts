517770d9446944eb5991152985d520745f34a850
AnthonyEMF <anthony07miranda@gmail.com>
2026-09-20 19:29:58 -0600

    Agregar sección de certificados

--- src/storage/educations.ts
+++ src/storage/educations.ts
@@ -1,9 +1,13 @@
+import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+import Aws from "../icons/tools/Aws.astro";
+import GoogleCloud from "../icons/tools/GoogleCloud.astro";
+
 export interface Education {
   title: string;
   date: string;
   description: string;
   link: string;
-  logo: string;
+  logo: string | AstroComponentFactory;
 }
 
 export const educations : Education[] = [
@@ -16,9 +20,19 @@ export const educations : Education[] = [
   },
   {
     title: "Ingeniería en Sistemas",
-    date: "2022 - 2027",
+    date: "2022 - 2026",
     description: "Universidad Nacional Autónoma de Honduras",
     link: "https://www.unah.edu.hn",
     logo: "/images/logos/unah.webp",
+  },
+];
+
+export const certifications: Education[] = [
+  {
+    title: "Fundamentos de AWS: Cloud, Serverless y Operación",
+    date: "20 de Septiembre 2026",
+    description: "Commit Academy",
+    link: "https://www.commitacademy.io/app/verify-certificate/cert_yhe5UVqhmO",
+    logo: Aws,
   },
 ];
